1387d34a908e99c651339dc4d5c171fc3b708643
andrei12388 <robertandreib.up@gmail.com>
2025-10-02 19:55:16 +0800

    footer fix

--- src/app/Footer/page.tsx
+++ src/app/Footer/page.tsx
@@ -1,7 +1,56 @@
+import Image from 'next/image'
 import styles from './Footer.module.css'
+import facebookIcon from "./facebook.png";
+import instagramIcon from "./instagram.png";
+import tiktokIcon from "./tiktok.png";
+import githubIcon from "./github.png";
 
 export default function Footer(){
     return <div className={styles.card}>
         <h1 className="text-white-300 ml-10">©2025 Robert Andrei Bardoquillo/All Rights Reserved</h1>
+        <div className={styles.iconHolder}>
+    <a href="https://www.facebook.com/robertandrei.bardoquillo" target="_blank" rel="noopener noreferrer">
+  <Image
+    width={100}
+    height={100}
+    className={styles.icon}
+    src={facebookIcon}
+    alt="Facebook icon"
+  />
+</a>
+
+<a href="https://instagram.com/Andreibardoquillo" target="_blank" rel="noopener noreferrer">
+  <Image
+    width={100}
+    height={100}
+    className={styles.icon}
+    src={instagramIcon}
+    alt="Instagram icon"
+  />
+</a>
+
+<a href="https://www.tiktok.com/@.alyaskulot" target="_blank" rel="noopener noreferrer">
+  <Image
+    width={100}
+    height={100}
+    className={styles.icon}
+    src={tiktokIcon}
+    alt="TikTok icon"
+  />
+</a>
+
+<a href="https://github.com/Andrei12388" target="_blank" rel="noopener noreferrer">
+  <Image
+    width={100}
+    height={100}
+    className={styles.icon}
+    src={githubIcon}
+    alt="GitHub icon"
+  />
+</a>
+
         </div>
+        
+                 </div>
+        
 }
